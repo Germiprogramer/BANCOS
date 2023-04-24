ac9db9e622719ddaaa9c3f9a10736128bc727aa9
Germiprogramer <llorentemunozgerman@gmail.com>
2023-04-24 23:09:57 +0200

    a

--- casino/casino.py
+++ casino/casino.py
@@ -2,34 +2,18 @@ from classes import *
 
 
 # crear 4 hilos de usuarios que jueguen al casino con numeros al aza
-def main():
-
-    casino = Casino()
-
-    usuario1 = Usuario("Pepe")
-    usuario2 = Usuario("Juan")
-    usuario3 = Usuario("Luis")
-    usuario4 = Usuario("Ana")
-
-    hilo1 = threading.Thread(target=usuario1.apostar, args=(10, "numero", casino))
-    hilo2 = threading.Thread(target=usuario2.apostar, args=(10, "numero", casino))
-    hilo3 = threading.Thread(target=usuario3.apostar, args=(10, "par/impar", casino))
-    hilo4 = threading.Thread(target=usuario4.apostar, args=(10, "martingala", casino))
-    
-    hilo1.start()
-    hilo2.start()
-    hilo3.start()
-    hilo4.start()
-    
-    hilo1.join()
-    hilo2.join()
-    hilo3.join()
-    hilo4.join()
-   
-
-    print("Saldo final: %d" % casino.getSaldo())
-
-
-
 if __name__ == "__main__":
-    main()
+    # crear objeto casino
+    casino = Casino()
+    # crear 4 objetos usuario y arrancar los hilos
+    usuarios = []
+    for i in range(4):
+        usuario = Usuario("usuario"+str(i+1), random.randint(0, 36), random.choice(["numero", "par/impar", "martingala"]), casino)
+        usuarios.append(usuario)
+        usuario.start()
+    # arrancar hilo del casino
+    casino.start()
+    # esperar a que todos los hilos terminen
+    for usuario in usuarios:
+        usuario.join()
+    casino.join()
--- casino/classes.py
+++ casino/classes.py
@@ -12,20 +12,25 @@ class Casino(threading.Thread):
         if apuesta == self.numero:
             usuario.setDinero(usuario.getDinero() + 360)
             self.saldo -= 360
+            print("{}".format(usuario.nombre)  + " ha ganado " + "{}".format(360) + "€")
         else:
             usuario.setDinero(usuario.getDinero() - 10)
             self.saldo += 10
+            print("{}".format(usuario.nombre)  + " ha perdido " + "{}".format(10) + "€")
 
     def parImpar(self, usuario, apuesta):
         if apuesta == "par" and self.numero % 2 == 0:
             usuario.setDinero(usuario.getDinero() + 20)
             self.saldo -= 20
+            print("{}".format(usuario.nombre)  + " ha ganado " + "{}".format(20) + "€")
         elif apuesta == "impar" and self.numero % 2 != 0:
             usuario.setDinero(usuario.getDinero() + 20)
             self.saldo -= 20
+            print("{}".format(usuario.nombre)  + " ha ganado " + "{}".format(20) + "€")
         else:
             usuario.setDinero(usuario.getDinero() - 10)
             self.saldo += 10
+            print("{}".format(usuario.nombre)  + " ha perdido " + "{}".format(10) + "€")
 
     def martingala(self, usuario, apuesta):
         apuesta = "fallida"
@@ -34,32 +39,45 @@ class Casino(threading.Thread):
             if apuesta == self.numero:
                 usuario.setDinero(usuario.getDinero() + 360*n)
                 self.saldo -= 360*n
+                print("{}".format(usuario.nombre)  + " ha ganado " + "{}".format(360*n) + "€")
             else:
                 usuario.setDinero(usuario.getDinero() - 10*n)
                 self.saldo += 10*n
+                print("{}".format(usuario.nombre)  + " ha perdido " + "{}".format(10*n) + "€")
                 n += 1
 
     def getSaldo(self):
         return self.saldo
+    
+    def run(self):
+        while self.saldo>0:
+            time.sleep(3)
+            self.numero = random.randint(0, 36)
+            print("El numero ganador es: " + "{}".format(self.numero))
+
         
 
 class Usuario(threading.Thread):
     
-        def __init__(self, nombre):
+        def __init__(self, nombre, numero, tipodeapuesta, casino):
             self.nombre = nombre
             self.dinero = 1000
+            self.numero = numero
+            self.tipodeapuesta = tipodeapuesta
+            self.casino = casino
 
-        def apostar(self, numero, tipodeapuesta, casino):
-            if tipodeapuesta == "numero":
-                casino.numeroGanador(Usuario, numero)
+        def run(self):
+            while self.dinero > 0:
+                if self.tipodeapuesta == "numero":
+                    self.casino.numeroGanador(self, self.numero)
 
-            elif tipodeapuesta == "par/impar":
-                casino.parImpar(Usuario, numero)
+                elif self.tipodeapuesta == "par/impar":
+                    self.casino.parImpar(self, self.numero)
 
-            elif tipodeapuesta == "martingala":
-                casino.martingala(Usuario, numero)
-            
-            time.sleep(3)
+                elif self.tipodeapuesta == "martingala":
+                    self.casino.martingala(self, self.numero)
+                
+                time.sleep(3)
     
         def getDinero(self):
             return self.dinero
